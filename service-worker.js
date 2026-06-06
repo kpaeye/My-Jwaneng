41afa7e18f3259dd05e728e8bef24505011b7012
kpaeye <kptsebe@gmail.com>
2026-06-06 19:28:27 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "myjwaneng-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/jobs.html",
+  "/news.html",
+  "/admin.html"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
