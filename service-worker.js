d9dd7dc97a7954ba2b40619a6b5cdf4334f461d4
kpaeye <kptsebe@gmail.com>
2026-07-02 19:09:59 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -19,9 +19,8 @@ self.addEventListener("install", function(event) {
 
 self.addEventListener("fetch", function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
 });
